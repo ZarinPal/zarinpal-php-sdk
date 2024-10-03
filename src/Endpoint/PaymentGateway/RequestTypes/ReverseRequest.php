7ffb117e01cbec8fe2b7dea725b08e5dfd8bf68b
ar4min <00001212aa@gmail.com>
2024-10-03 18:10:38 +0330

    Add Validator Class

--- src/Endpoint/PaymentGateway/RequestTypes/ReverseRequest.php
+++ src/Endpoint/PaymentGateway/RequestTypes/ReverseRequest.php
@@ -2,9 +2,8 @@
 
 namespace ZarinPal\Sdk\Endpoint\PaymentGateway\RequestTypes;
 
-use InvalidArgumentException;
 use ZarinPal\Sdk\Endpoint\Fillable;
-use ZarinPal\Sdk\Options;
+use ZarinPal\Sdk\Validator;
 
 class ReverseRequest
 {
@@ -15,22 +14,8 @@ class ReverseRequest
 
     public function validate(): void
     {
-        $this->validateMerchantId();
-        $this->validateAuthority();
-    }
-
-    private function validateMerchantId(): void
-    {
-        if ($this->merchantId === null || !preg_match('/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/', $this->merchantId)) {
-            throw new InvalidArgumentException('Invalid merchant_id format. It should be a valid UUID.');
-        }
-    }
-
-    private function validateAuthority(): void
-    {
-        if (!preg_match('/^A[0-9a-zA-Z]{35}$/', $this->authority)) {
-            throw new InvalidArgumentException('Invalid authority format. It should be a string starting with "A" followed by 32 alphanumeric characters.');
-        }
+        Validator::validateMerchantId($this->merchantId);
+        Validator::validateAuthority($this->authority);
     }
 
     final public function toString(): string
--- src/Endpoint/PaymentGateway/RequestTypes/VerifyRequest.php
+++ src/Endpoint/PaymentGateway/RequestTypes/VerifyRequest.php
@@ -2,9 +2,9 @@
 
 namespace ZarinPal\Sdk\Endpoint\PaymentGateway\RequestTypes;
 
-use InvalidArgumentException;
 use JsonException;
 use ZarinPal\Sdk\Endpoint\Fillable;
+use ZarinPal\Sdk\Validator;
 
 class VerifyRequest
 {
@@ -16,30 +16,9 @@ class VerifyRequest
 
     public function validate(): void
     {
-        $this->validateMerchantId();
-        $this->validateAmount();
-        $this->validateAuthority();
-    }
-
-    private function validateMerchantId(): void
-    {
-        if ($this->merchantId === null || !preg_match('/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/', $this->merchantId)) {
-            throw new InvalidArgumentException('Invalid merchant_id format. It should be a valid UUID.');
-        }
-    }
-
-    private function validateAmount(): void
-    {
-        if ($this->amount <= 0) {
-            throw new InvalidArgumentException('Amount must be greater than zero.');
-        }
-    }
-
-    private function validateAuthority(): void
-    {
-        if (!preg_match('/^A[0-9a-zA-Z]{35}$/', $this->authority)) {
-            throw new InvalidArgumentException('Invalid authority format. It should be a string starting with "A" followed by 32 alphanumeric characters.');
-        }
+        Validator::validateMerchantId($this->merchantId);
+        Validator::validateAmount($this->amount);
+        Validator::validateAuthority($this->authority);
     }
 
     /**
